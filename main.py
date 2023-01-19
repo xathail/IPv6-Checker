57410abdd0c160622a4a378032c897baa2063520
xzerios <108062540+xzerios@users.noreply.github.com>
2023-01-19 10:44:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ async def check(
     except:
         await ctx.respond(f':x: {domain} does not support IPv6')
 
-bot.run("MTA2NDU4MTcxNjc3OTQ3MDg4OQ.GQab44.dINa6h2ZIzy8OppQmI8BWEMAL3Ugn_5QDO6BZI")
+bot.run("TOKEN")
