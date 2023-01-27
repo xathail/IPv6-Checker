d39489094c7b30ea6fe2f9a44869bd4c48db0380
xzerios <108062540+xzerios@users.noreply.github.com>
2023-01-27 20:54:37 +0000

    Adds a ping command.

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ import requests
 
 bot = discord.Bot()
 
+@bot.command(description="Sends the bot's latency.")
+async def ping(ctx):
+    if round(bot.latency * 1000) <= 50:
+        embed=discord.Embed(title="Bot Ping:", description=f"The ping is **{round(bot.latency *1000)}** milliseconds!", color=0x44ff44)
+    elif round(bot.latency * 1000) <= 100:
+        embed=discord.Embed(title="Bot Ping:", description=f"The ping is **{round(bot.latency *1000)}** milliseconds!", color=0xffd000)
+    elif round(bot.latency * 1000) <= 200:
+        embed=discord.Embed(title="Bot Ping:", description=f"The ping is **{round(bot.latency *1000)}** milliseconds!", color=0xff6600)
+    else:
+        embed=discord.Embed(title="Bot Ping:", description=f"The ping is **{round(bot.latency *1000)}** milliseconds!", color=0x990000)
+    await ctx.respond(embed=embed)
+    
 @bot.command(description="Check a domain to see if it supports cloudflare.")
 async def cloudflare(ctx, domain: str):
     if 'https://' in domain or 'http://' in domain: 
@@ -28,7 +40,7 @@ async def cloudflare(ctx, domain: str):
 async def ipv6(ctx, domain: discord.Option(discord.SlashCommandOptionType.string)):
     try:
         ipv6_address = socket.getaddrinfo(domain, None, socket.AF_INET6)
-        embed = discord.Embed(title=f'{domain} Supports IPv6:', description=f'IPv6 Address: {ipv6_address[0][4][0]}', color=0x32CD32)
+        embed = discord.Embed(title=f'{domain} Supports IPv6', description=f'IPv6 Address: {ipv6_address[0][4][0]}', color=0x32CD32)
         await ctx.respond(embed=embed)
     except socket.gaierror as e:
         embed = discord.Embed(title=f'Error Finding Compatibility:', description=f'{e}', color=0xFF0000)
