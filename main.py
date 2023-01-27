4b94fd4bd3e8ea0633a79b137c902f703b6c1e3c
xzerios <108062540+xzerios@users.noreply.github.com>
2023-01-27 19:39:59 +0000

    Added Embeds
    Embeds now used instead of regular messages.

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import discord
 import socket
+
 bot = discord.Bot()
 
 @bot.command()
-async def check(
-  ctx,
-  domain: discord.Option(discord.SlashCommandOptionType.string)
-):
+async def check(ctx, domain: discord.Option(discord.SlashCommandOptionType.string)):
     try:
         ipv6_address = socket.getaddrinfo(domain, None, socket.AF_INET6)
-        await ctx.respond(f':white_check_mark: {domain} supports IPv6')
-    except:
-        await ctx.respond(f':x: {domain} does not support IPv6')
-
+        embed = discord.Embed(title=f'{domain} Supports IPv6:', description=f'IPv6 Address: {ipv6_address[0][4][0]}', color=0x32CD32)
+        await ctx.respond(embed=embed)
+    except socket.gaierror as e:
+        embed = discord.Embed(title=f'Error Finding Compatibility:', description=f'{e}', color=0xFF0000)
+        await ctx.respond(embed=embed)
 bot.run("TOKEN")
