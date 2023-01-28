8526743319dfe48469b55c2a1027e8547d846d56
xzerios <108062540+xzerios@users.noreply.github.com>
2023-01-28 11:46:00 +0000

    Combines Commands & Better Embeds
    Now runs a cf and ipv6 check - embeds look better

--- main.py
+++ main.py
@@ -1,37 +1,52 @@
+# Imports
 import discord
 import socket
 import requests
 
+# Setup Bot
 bot = discord.Bot(activity = discord.Game(name="Around With IPv6 | By xerius"), status=discord.Status.idle)
 print("Bot Online!")
 
-@bot.command(description="Sends the bot's latency.")
-async def ping(ctx):
-    embed = discord.Embed(title="Bot Ping:", description=f"The ping is **{round(bot.latency *1000)}** milliseconds!", color=0x44ff44 if round(bot.latency * 1000) <= 50 else 0xffd000 if round(bot.latency * 1000) <= 100 else 0xff6600 if round(bot.latency * 1000) <= 200 else 0x990000)
-    await ctx.respond(embed=embed)
-    
-@bot.command(description="Check a domain to see if it supports cloudflare.")
-async def cloudflare(ctx, domain: str):
-    domain = domain if 'https://' in domain or 'http://' in domain else 'http://'+domain
-    response = requests.get(domain)
-    embed = discord.Embed(title=f'{domain} Uses Cloudflare' if 'cf-ray' in response.headers else f'{domain} Does Not Use Cloudflare', color=0x32CD32 if 'cf-ray' in response.headers else 0xFF0000)
-    await ctx.respond(embed=embed)
-
-@bot.command(description="Check a domain to see if it supports IPv6.")
-async def check(ctx, domain: discord.Option(discord.SlashCommandOptionType.string)):
-    domain = domain.replace("http://","").replace("https://","").replace("www.","")
+# Check Command
+@bot.command(description="Checks IPv6 & Cloudflare Compatibility")
+async def check(ctx, domain: str):
+    domain = domain.replace("http://","").replace("https://","").replace("www.","") # Removes http://, https:// and www.
     try:
-        ipv6_address = socket.getaddrinfo(domain, None, socket.AF_INET6)
-        embed = discord.Embed(title=f'{domain} Supports IPv6', description=f'IPv6 Address: {ipv6_address[0][4][0]}', color=0x32CD32)
-        await ctx.respond(embed=embed)
-    except socket.gaierror as e:
-        domain = 'www.'+domain
+        ipv6_address = socket.getaddrinfo(domain, None, socket.AF_INET6) # Tries To Get IPv6 Address
+        domain = "http://"+domain # Setup For CF Request
+        response = requests.get(domain)
+        if 'cf-ray' in response.headers: # If Site Uses Cloudflare
+            embed = discord.Embed(title=f"{domain} Information", description=f"\n:white_check_mark: IPv6\n:white_check_mark: Cloudflare", color=0x32CD32)
+            embed.set_footer(text=f"IPv6 Address: {ipv6_address[0][4][0]}")
+            await ctx.respond(embed=embed)
+        else: # If Site Doesn't Use Cloudflare
+            embed = discord.Embed(title=f"{domain} Information", description=f"\n:white_check_mark: IPv6\n:white_check_mark: Cloudflare", color=0x32CD32)
+            embed.set_footer(text=f"IPv6 Address: {ipv6_address[0][4][0]}")
+            await ctx.respond(embed=embed)
+    except socket.gaierror as e: # If An Error Occures Trying To Get IPv6 Address
+        domain = 'www.'+domain 
         try:
             ipv6_address = socket.getaddrinfo(domain, None, socket.AF_INET6)
-            embed = discord.Embed(title=f'{domain} Supports IPv6', description=f'IPv6 Address: {ipv6_address[0][4][0]}', color=0x32CD32)
-            await ctx.respond(embed=embed)
-        except socket.gaierror as e:
-            embed = discord.Embed(title=f'Error Finding Compatibility', description=f'I was unable to find IPv6 compatibility for the domain {domain} \n\n*{e}*', color=0xFF0000)
-            await ctx.respond(embed=embed)
-
+            domain = domain.replace("www.","http://")
+            response = requests.get(domain)
+            if 'cf-ray' in response.headers:
+                embed = discord.Embed(title=f"{domain} Information", description=f"\n:white_check_mark: IPv6\n:white_check_mark: Cloudflare", color=0x32CD32)
+                embed.set_footer(text=f"IPv6 Address: {ipv6_address[0][4][0]}")
+                await ctx.respond(embed=embed)
+            else:
+                embed = discord.Embed(title=f"{domain} Information", description=f"\n:white_check_mark: IPv6\n:white_check_mark: Cloudflare", color=0x32CD32)
+                embed.set_footer(text=f"IPv6 Address: {ipv6_address[0][4][0]}")
+                await ctx.respond(embed=embed)
+        except socket.gaierror as e: # If There Is No IPv6 Address
+            domain = domain.replace("www.","http://")
+            response = requests.get(domain)
+            if 'cf-ray' in response.headers:
+                embed = discord.Embed(title=f"{domain} Information", description=f"\n:x: IPv6\n:white_check_mark: Cloudflare", color=0xFF0000)
+                embed.set_footer(text=f"{e}")
+                await ctx.respond(embed=embed)
+            else:
+                embed = discord.Embed(title=f"{domain} Information", description=f"\n:x: IPv6\n:x: Cloudflare", color=0xFF0000)
+                embed.set_footer(text=f"{e}")
+                await ctx.respond(embed=embed)
+# Runs Bot
 bot.run("TOKEN")
